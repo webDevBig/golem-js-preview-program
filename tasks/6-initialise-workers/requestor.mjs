a0223e343aa1be3b16e0a381f9da31e98bcb4b62
DEVBIG <olenadev7@gmail.com>
2023-10-30 15:07:20 +0200

    add task 6

--- tasks/6-initialise-workers/requestor.mjs
+++ tasks/6-initialise-workers/requestor.mjs
@@ -3,10 +3,9 @@ import * as fs from 'fs';
 
 (async () => {
   const executor = await TaskExecutor.create({
-    package: "a7db2f25445de01650a62ffcfb35219ed48f014ece83cfea15c36b0f",    
+    package: "a7db2f25445de01650a62ffcfb35219ed48f014ece83cfea15c36b0f",
     yagnaOptions: { apiKey: '7eb7bce657144200b75f562c82088b74' },
     maxParallelTasks: 3,
-    taskVolume: { '/golem/input': { model: 'test/data' } },
   });
 
   const words = fs.readFileSync('test-data.txt', 'utf8').split('\n').map(word => word.trim());
@@ -23,11 +22,11 @@ import * as fs from 'fs';
 
   await executor.forEach(chunks, async (ctx, item) => {
     const chunkText = item.join('\n');
-   await ctx
-      .beginBatch()
-      .run('python3', ['similarities.py', '-w', chunkText, '-d', '/golem/input/worldcities.csv'])
-      .end();
+    console.log(`Sending task to ${ctx.provider.name}. Data: ${chunkText}`);
+    await ctx.run('/usr/local/bin/python3', ['similarities.py', '-w', chunkText, '-d', '/golem/input/worldcities.csv']);
+    console.log(`Task sent to ${ctx.provider.name}`);
   });
   
   await executor.end(); 
+  console.log('Executor has finished');
 })();
