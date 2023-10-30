1a6816a77b038ecfb74f41379bec44d23daf4c24
DEVBIG <olenadev7@gmail.com>
2023-10-30 14:44:21 +0200

    add task 5

--- tasks/5-run-tasks-in-parallel/requestor.mjs
+++ tasks/5-run-tasks-in-parallel/requestor.mjs
@@ -19,61 +19,56 @@ const imagesPath = program.opts().images;
     }
   });
 
+  try {
+    const folderName = await fs.promises.readdir(imagesPath);
 
-  const folderName = await fs.promises.readdir(imagesPath);
+    const imagesData = folderName.map((image) => {
+        const [name, ext] = image.split(".");
+        if (ext == "jpg" || ext == "png" || ext == "jpeg") {
+          return {
+            path: `./images/${image}`,
+            name,
+            ext,
+          };
+        } else {
+          return;
+        }
+      }).filter((image) => image && !image.name.startsWith("upscaled-"));
 
-  const imagesData = folderName
-    .map((image) => {
-      const [name, ext] = image.split(".");
-      if (ext == "jpg" || ext == "png" || ext == "jpeg") {
-        return {
-          path: `./images/${image}`,
-          name,
-          ext,
-        };
-      } else {
-        return;
-      }
-    })
-    .filter((image) => image && !image.name.startsWith("upscaled-"));
+    if (!folderName) {
+      console.error("Please provide a valid path to the images directory");
+      process.exit(1);
+    }
+    if (imagesData.length === 0) {
+      console.error("No images to process");
+      process.exit(1);
+    }
 
-  if (!folderName) {
-    console.error("Please provide a valid path to the images directory");
-    process.exit(1);
-  }
-
-
-  if (!imagesData.length) {
-    console.error("No images to process");
-    process.exit(1);
-  }
-  console.log(`Found ${imagesData.length} images to process`);
-
-  for (const image of imagesData) {
-    try {
-      await executor.run(async (ctx) => {
+    await executor.forEach(imagesData, async (ctx, image) => {
+      try {
         if (!fs.existsSync(image.path)) {
           console.error(`File not found: ${image.path}`);
           return;
         }
-
         let fileName = path.basename(image.path);
 
-        ctx.uploadFile(image.path, `/golem/work/${fileName}`);
+        await ctx.uploadFile(image.path, `/golem/work/${fileName}`);
+
         const output = await ctx.run(`realesrgan-ncnn-vulkan -i /golem/work/${fileName} -o /golem/work/upscaled-${fileName} -n realesr-animevideov3-x2 -s 2 -f .jpg`);
 
         if (output.stderr && output.stderr.includes("decode image")) {
           console.error(`Error decoding image: ${output.stderr}`);
-
         } else if (output.isBatchFinished) {
           await ctx.downloadFile(`/golem/work/upscaled-${fileName}`, `${imagesPath}/upscaled/upscaled-${fileName}`);
           console.log(output);
         }
-      });
-    } catch (err) {
-      console.error(`Task execution error: ${err}`);
-    }
+      } catch (err) {
+        console.error(`Task execution error: ${err}`);
+      }
+    });
+  } catch (err) {
+    console.error("Error reading the images directory:", err);
+    process.exit(1);
   }
-
   await executor.end();
 })();
