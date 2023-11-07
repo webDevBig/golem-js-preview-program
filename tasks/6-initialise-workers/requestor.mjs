0de1ac789092538a9dd1f07865412a732d7e3591
DEVBIG <olenadev7@gmail.com>
2023-11-07 13:36:08 +0200

    add task 6

--- tasks/6-initialise-workers/requestor.mjs
+++ tasks/6-initialise-workers/requestor.mjs
@@ -6,25 +6,22 @@ import * as fs from 'fs';
     package: "a7db2f25445de01650a62ffcfb35219ed48f014ece83cfea15c36b0f",
     yagnaOptions: { apiKey: '7eb7bce657144200b75f562c82088b74' },
     maxParallelTasks: 3,
+    taskTimeout: 5 * 60 * 1000, 
   });
 
   const words = fs.readFileSync('test-data.txt', 'utf8').split('\n').map(word => word.trim());
-  const chunkSize = Math.ceil(words.length / 3);
-  const chunks = [];
-  for (let i = 0; i < words.length; i += chunkSize) {
-    chunks.push(words.slice(i, i + chunkSize));
-  }
+  
 
   executor.beforeEach(async (ctx) => {
     console.log(ctx.provider.name + " is downloading worldcities.csv file");
     await ctx.uploadFile('worldcities.csv', '/golem/input/worldcities.csv');
   });
 
-  await executor.forEach(chunks, async (ctx, item) => {
-    const chunkText = item.join('\n');
-    console.log(`Sending task to ${ctx.provider.name}. Data: ${chunkText}`);
-    await ctx.run('/usr/local/bin/python3', ['similarities.py', '-w', chunkText, '-d', '/golem/input/worldcities.csv']);
-    console.log(`Task sent to ${ctx.provider.name}`);
+  await executor.forEach(words, async (ctx, word) => {
+    console.log(`Sending task for word to ${ctx.provider.name}. Data: ${word}`);
+    const result = await ctx.run('/usr/local/bin/python3', ['similarities.py', '-w', word, '-d', '/golem/input/worldcities.csv']);
+    console.log(`Task sent for word to ${ctx.provider.name}`);
+    console.log(`Result for word ${word}: ${result.stdout}`);
   });
   
   await executor.end(); 
